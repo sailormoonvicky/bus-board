3f9f8266366fb7e707e83632ad741596db36f2f7
sailormoonvicky <victoriahuhh@gmail.com>
2024-01-18 16:10:23 +0000

    implete part2 with bugs

--- app.js
+++ app.js
@@ -17,12 +17,10 @@ async function busInfo(id) {
         return a.timeToStation - b.timeToStation;
     })
 
-    // const arrivalBuses = [];
     if (sortedData.length >= 5) {
         for (let i = 0; i < 5; i++) {
             let bus = sortedData[i];
             console.log(`The bus ${bus.lineName} towards ${bus.towards} will come at the station ${bus.stationName} in ${Math.floor(bus.timeToStation/60)} mins, the destination is ${bus.destinationName}. `);
-            // arrivalBuses.push([bus[lineName], bus[towards], bus[stationName], Math.floor(bus[timeToStation]/60), bus[destinationName]])
         }
     } else {
         sortedData.forEach(bus => {
@@ -31,7 +29,7 @@ async function busInfo(id) {
     }
 }
 
-busInfo('490008660N');
+// busInfo('490008660N');
 
 
 // Part 2
@@ -39,3 +37,52 @@ busInfo('490008660N');
 // given a postcode, finds the two nearest bus stops, 
 // and shows the next five buses due at each.
 
+async function getBusStop (postcode) {
+    try {
+        const url = `https://api.postcodes.io/postcodes/${postcode}`;
+        const response = await fetch(url, {method:'GET'});
+        const data = await response.json();
+        const lat = data.result.latitude;
+        const lon = data.result.longitude;
+
+        const tflURL = `https://api.tfl.gov.uk/StopPoint/?lat=${lat}&lon=${lon}7&stopTypes=NaptanPublicBusCoachTram&radius=1000`;
+
+        const tflResponse = await fetch (tflURL, {method: 'GET'});
+        // console.log(tflResponse.status);
+        const stops = await tflResponse.json();
+
+        const result = new Array(2);
+        result[0] = stops.stopPoints[0].naptanId;
+        result[1] = stops.stopPoints[1].naptanId;
+        // console.log(result);
+
+        return result;
+
+    } catch (error) {
+        console.error (`Download error: ${error.message}`);
+    }
+}
+
+// getBusStop('NW3 5RL');
+
+async function postalBus (postcode) {
+
+    try {
+        const stopIds = await getBusStop(postcode);
+
+        if (stopIds && stopIds >= 2) {
+            console.log(`The info about the nearst bus station: `);
+            await busInfo(stopIds[0]);
+        
+            console.log(`The info about the second nearst bus station: `);
+            await busInfo(stopIds[1]);
+        } else {
+            console.log('Unable to find nearest bus stops.');
+        }
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+    }
+
+}
+
+postalBus('NW3 5RL');
