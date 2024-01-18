b483871f77f6934c920fc21d8f62838055486e1b
sailormoonvicky <victoriahuhh@gmail.com>
2024-01-18 12:20:02 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import fetch from 'node-fetch';
+// Part 1
+//  Your program should ask the user for a stop code, 
+// and print a list of the next five buses at that stop code, 
+// with their routes, destinations, 
+// and the time until they arrive in minutes.
+
+
+
+
+// Your aim is to build an application which, 
+// given a postcode, finds the two nearest bus stops, 
+// and shows the next five buses due at each.
